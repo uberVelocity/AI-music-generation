d57629350067f98246b475bc2a3704aec4fa07c4
ubervelocity <mihaitu.popescu@gmail.com>
2020-03-12 10:35:49 +0100

    Init repository.
    Added conversion of midi files to note sequences script.

---
+++ musicVAE/midi2notes.sh
@@ -0,0 +1,7 @@
+INPUT_DIRECTORY=$1
+SEQUENCES_TFRECORD=/tmp/notesequences.tfrecord
+
+convert_dir_to_note_sequences \
+  --input_dir=$INPUT_DIRECTORY \
+  --output_file=$SEQUENCES_TFRECORD \
+  --recursive
---
+++ musicVAE/network.py
@@ -0,0 +1,12 @@
+import magenta
+import magenta.music as mm
+import tensorflow
+
+# Import dependencies.
+from magenta.models.music_vae import configs
+from magenta.models.music_vae.trained_model import TrainedModel
+
+# Initialize the model.
+print("Initializing Music VAE...")``
+# music_vae = TrainedModel(configs.CONFIG_MAP['cat-mel_2bar_big'], batch_size = 4)
+print('Done!')
