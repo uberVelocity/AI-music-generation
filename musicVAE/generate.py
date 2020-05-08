d7b114d016216b75eb5168cf1988b976fe01f4fb
ubervelocity <mihaitu.popescu@gmail.com>
2020-05-08 19:59:52 +0200

    added best songs and generated songs from three networks'

--- musicVAE/generate.py
+++ musicVAE/generate.py
@@ -1,7 +1,7 @@
 import subprocess
 # /tmp/music_vae/train/model.ckpt.
 def generate_music():
-    generate = 'music_vae_generate --config=cat-mel_2bar_small --checkpoint_file=/home/merkel/Documents/University/DL/train/2bar_small_32_0005/model.ckpt-2014 --mode=sample  --num_outputs=5  --output_dir=/home/merkel/Documents/University/DL/generated/2bar_small_32_0005'
+    generate = 'music_vae_generate --config=cat-mel_2bar_big --checkpoint_file=/home/merkel/Documents/University/DL/music/musicVAE/models/2bar_big_batch32_learning00005/model.ckpt-104 --mode=sample  --num_outputs=100  --output_dir=/home/merkel/Documents/University/DL/generated/2bar_big_batch32_learning00005'
     print('Generating music using hierdec-mel_16bar and trained model...')
     process = subprocess.Popen(generate.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
